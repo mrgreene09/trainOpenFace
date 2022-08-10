a125947665f886b577431eb3c63aba6e055dc6d7
joaquinmtorres <joaquinmercadotorres@gmail.com>
2022-08-10 09:42:35 -0400

    Added loops

--- cropFaces.py
+++ cropFaces.py
@@ -8,20 +8,37 @@ Created on Tue Aug  9 10:28:49 2022
 
 # Import libraries
 import cv2 
+import glob
+from mtcnn.mtcnn import MTCNN
+
+# Helper functions
+def face_detect(img):
+    detector = MTCNN()
+    # detect faces in the image
+    faces = detector.detect_faces(img)
+    return  faces  
 
 # this is a test
 # Get input from user about which folder to start
 ## define where raw frames are
+rawFrames = "/Volumes/etna/Scholarship/Michelle Greene/Shared/AminaThesis/rawFrames/"
 ## define where crops are
+cropFrames = "/Volumes/etna/Scholarship/Michelle Greene/Shared/AminaThesis/cropFrames/"
+
+dirList = sorted(glob.glob(rawFrames+ '*'))
 
 # Loop through each folder beginning with start folder
+for i in dirList:
+    ethList = sorted(glob.glob(i + "/" + "*"))
 # Loop through all video folders
+    for j in ethList:
+        imageList = sorted(glob.glob(j + "/" + "*.jpg"))
 # Loop through each image
-
-# Open the image
-img = cv2.imread('some/path/to/fix') #REPLACE ME
-
-# Detect the face
+        for k in imageList:
+            # Open the image
+            img = cv2.imread(k)
+            # Detect the face
+            face_detect(img)            
 
 # Crop the face
 
