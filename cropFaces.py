21c5a534aa120ed7be94e2d222c678633ee25bc1
joaquinmtorres <joaquinmercadotorres@gmail.com>
2022-08-22 09:44:46 -0400

    Added working manual crop

--- cropFaces.py
+++ cropFaces.py
@@ -10,7 +10,6 @@ Created on Tue Aug  9 10:28:49 2022
 import cv2 
 import glob
 from mtcnn.mtcnn import MTCNN
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import RectangleSelector
 import os
@@ -44,15 +43,18 @@ def onkeypress(event):
     global tl_list
     global br_list
     if event.key == 'q':
-        tl_list = np.array(tl_list[0])
-        br_list = np.array(br_list[0])
-        x, y, x2, y2 = tl_list[0], tl_list[1], br_list[0], br_list[1]
+        # Save coordinates
+        x, y, x2, y2 = tl_list[0][0], tl_list[0][1], br_list[0][0], br_list[0][1]
+        # Crop image
         crop = img[y:y2, x:x2]
         crop = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
         # Save cropped image
-        saveName = 'face'+str(frameCount)
-        cv2.imwrite(os.path.join(path, saveName)+'.jpg', crop)
+        saveName = 'faceTest2'
+        cv2.imwrite('/Users/bcvl/Desktop/' + saveName + '.jpg', crop)
         plt.close()
+        # Reset values for the next image
+        tl_list = []
+        br_list = []
 
 # Get input from user about which folder to start
 ## define where raw frames are
@@ -76,10 +78,6 @@ for i in dirList:
         # Loop through each image
         for k in imgList:
             frameCount += 1
-                
-            # Reset values
-            tr_list = []
-            br_list = []
             
             # extract ethnic group name
             ethnicGroup = i.split('/')[-1] 
@@ -95,9 +93,10 @@ for i in dirList:
             saveName = 'face'+str(frameCount)
             newCrop = os.path.join(path, saveName + '.jpg')
             
-            # Does the image file exist already?
+            # Skip existing files
             if os.path.isfile(newCrop) == True:
                 continue
+            
             else:
                 # Open the image
                 print('Starting {}'.format(prelimName[:-4]) + ' in {}'.format(j.split('/')[-1]))
@@ -106,6 +105,7 @@ for i in dirList:
                 # Detect the face
                 faces = face_detect(img) 
                 
+                # MTCNN detects a face
                 if len(faces) > 0:
                     x, y, width, height = (faces[0]['box'])
                     
@@ -119,39 +119,40 @@ for i in dirList:
                     
                     # Save image
                     cv2.imwrite(os.path.join(path, saveName)+'.jpg', crop)
-                    
-                # Temporary
+                
+                # MTCNN doesn't detect a face    
                 else:
-                    continue
+                    # Skip existing files
+                    if os.path.isfile(newCrop) == True:
+                        continue
                     
-                #else:
-                    #if os.path.isfile(newCrop) == True:
-                        #continue
-                    #else:
-                        #fig, ax = plt.subplots(1)
-                        #mngr = plt.get_current_fig_manager()
-                        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                        #ax.imshow(img)
+                    else:
+                        # Open image for assessment
+                        fig, ax = plt.subplots(1)
+                        mngr = plt.get_current_fig_manager()
+                        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                        ax.imshow(img)
                         
-                        #plt.show()
+                        plt.show()
                         
-                        #facePresent = input('Is there a face? [y/n]: ')
+                        # Assess whether there's a face
+                        facePresent = input('Is there a face? [y/n]: ')
                         
-                        #if facePresent == 'y':
+                        if facePresent == 'y':
                             # Create bounding box
-                            #fig, ax = plt.subplots(1)
-                            #mngr = plt.get_current_fig_manager()
-                            #ax.imshow(img)
+                            fig, ax = plt.subplots(1)
+                            mngr = plt.get_current_fig_manager()
+                            ax.imshow(img)
                             
-                            #toggle_selector.RS = RectangleSelector(
-                                #ax, line_select_callback, 
-                                #useblit=True,
-                                #button=[1], minspanx=5, minspany=5,
-                                #spancoords='pixels', interactive=True
-                                #)
-                            #bbox = plt.connect('key_press_event', toggle_selector)
-                            #key = plt.connect('key_press_event', onkeypress)
+                            toggle_selector.RS = RectangleSelector(
+                                ax, line_select_callback, 
+                                useblit=True,
+                                button=[1], minspanx=5, minspany=5,
+                                spancoords='pixels', interactive=True
+                                )
+                            bbox = plt.connect('key_press_event', toggle_selector)
+                            key = plt.connect('key_press_event', onkeypress)
                             
-                            #plt.show()
+                            plt.show()
                    
             cv2.destroyAllWindows()
