3aca27e6706bb23f2dec006a4ee593a94e3b21bd
joaquinmtorres <joaquinmercadotorres@gmail.com>
2022-08-24 10:27:04 -0400

    Optimized manual crop

--- testCrop.py
+++ testCrop.py
@@ -9,20 +9,20 @@ Created on Fri Aug 12 09:38:32 2022
 import cv2 
 import matplotlib.pyplot as plt
 from matplotlib.widgets import RectangleSelector
-import numpy as np
+import glob
 
 # Global constants
 tl_list = []
 br_list = []
 
-## Bounding box
+## Coordinates of bounding box
 def line_select_callback(clk, rls):
     global tl_list
     global br_list
     tl_list.append((int(clk.xdata), int(clk.ydata)))
     br_list.append((int(rls.xdata), int(rls.ydata)))
 
-## Bounding box    
+## Drawing bounding box    
 def toggle_selector(event):
     toggle_selector.RS.set_active(True)
     
@@ -37,32 +37,29 @@ def onkeypress(event):
         crop = img[y:y2, x:x2]
         crop = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
         # Save cropped image
-        saveName = 'faceTest2'
+        saveName = 'faceTest' + str(frameCount)
         cv2.imwrite('/Users/bcvl/Desktop/' + saveName + '.jpg', crop)
         plt.close()
         # Reset values for the next image
         tl_list = []
         br_list = []
-    
-img = cv2.imread('/Volumes/etna/Scholarship/Michelle Greene/Shared/AminaThesis/rawFrames/E9/calinalawrence_E9_A3_flink 1/frame2.jpg')
 
-# Open image for assessment
-fig, ax = plt.subplots(1)
-mngr = plt.get_current_fig_manager()
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-ax.imshow(img)
+dirList = '/Volumes/etna/Scholarship/Michelle Greene/Shared/AminaThesis/rawFrames/E9/calinalawrence_E9_A3_flink 1/'
 
-plt.show()
+imgList = sorted(glob.glob(dirList + '*.jpg'))
 
-# Assess if there's a face
-facePresent = input('Is there a face? [y/n]: ')
+frameCount = 0
 
-if facePresent == 'y':
-    # Create bounding box
+for i in imgList:
+    frameCount += 1
+    img = cv2.imread(i)
+
+    # Open image for assessment
     fig, ax = plt.subplots(1)
     mngr = plt.get_current_fig_manager()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     ax.imshow(img)
-    
+        
     toggle_selector.RS = RectangleSelector(
         ax, line_select_callback,
         useblit=True,
@@ -71,5 +68,4 @@ if facePresent == 'y':
         )
     bbox = plt.connect('key_press_event', toggle_selector)
     key = plt.connect('key_press_event', onkeypress)
-    
     plt.show()
